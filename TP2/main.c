5fc6525d9cb49b9017dce4108a609857e52744d0
EmanuelAl <52534676+EmanuelAl@users.noreply.github.com>
2019-09-22 23:54:31 -0300

    Add files via upload

---
+++ TP2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct
+{
+ int id;
+ char name[51];
+ char lastName[51];
+ float salary;
+ int sector;
+ int isEmpty;
+}typedef Employee;
+
+int main()
+{
+
+    return 0;
+}
+int initEmployees(Employee* list, int len)
+{
+
+}
