d21b042d8f78a6ba3994ac1c330adbc13718277a
EmanuelAl <52534676+EmanuelAl@users.noreply.github.com>
2019-10-04 22:18:21 -0300

    Add files via upload

--- TP2/main.c
+++ TP2/main.c
@@ -33,6 +33,14 @@ int hardcodeoEmployees(Employee employees[],int len,int cant);
 
 int menuSort();
 int menu();
+void informsEmployees(Employee employees[], int len);
+
+float totalSalary(Employee e[],int len);
+void showEmployeeSalary(Employee x);
+void printEmployeesSalary(Employee list[],int len);
+float getAverageSalary(Employee e[],int len);
+int numExceedPromedio(Employee e[],int len);
+int menuInforms();
 
 int main()
 {
@@ -82,7 +90,7 @@ int main()
             break;
         case 6:
             printf("Informs \n");
-            //
+            informsEmployees(list,TAMe);
             break;
 
         case 7:
@@ -132,7 +140,66 @@ int menuSort()
     scanf("%d",&option);
     return option;
 }
+int menuInforms()
+{
+    int option;
 
+    system("cls");
+    printf("****** Informs Employees *******\n\n");
+    printf("1-Show list of Employees sorted alphabetically by last name and sector\n");
+    printf("2-Show total salary , average wages ,how many employees exceed the average salary \n");
+    printf("5- Get out\n");
+    printf(" Enter option:");
+    scanf("%d",&option);
+
+    return option;
+}
+void informsEmployees(Employee employees[], int len)
+{
+    char salir = 'n';
+    system("cls");
+    printf("***** Informs Employees ****\n\n");
+
+    do
+    {
+        switch( menuInforms())
+        {
+        case 1:
+            sortEmployeesByLastNameSector(employees,len,1);
+            printEmployees(employees,len);
+            break;
+
+        case 2:
+            system("cls");
+            printEmployeesSalary(employees,len);
+            printf(" \n total salary: %.2f \n",totalSalary(employees,len));
+            printf(" The average salary  is : %.2f \n ",getAverageSalary(employees,len));
+            printf(" Exceed average %d \n",numExceedPromedio(employees,len));
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+             printf("Confirm get out?¿ \n");
+            fflush(stdin);
+            salir=getchar();
+            //
+            break;
+
+        default:
+            printf("Invalid option \n");
+
+        }
+        system("pause");
+    }
+    while(salir == 'n');
+}
 
 int initEmployees(Employee list[], int len)
 {
@@ -153,6 +220,39 @@ void showEmployee(Employee x)//muestra un empleado
            x.lastName,
            x.salary,
            x.sector);
+}
+void showEmployeeSalary(Employee x)//muestra un empleado
+{
+    printf(" %2d  %10s   %12s         %.2f \n",
+
+           x.id,
+           x.name,
+           x.lastName,
+           x.salary
+           );
+}
+void printEmployeesSalary(Employee list[],int len)//muesstra los empleados
+{
+
+    int flag=0;
+    system("cls");
+    printf("******* list of Employees whit Salary ********\n");
+    printf(" id         name    last name           salary \n");
+
+    for(int i=0; i<len; i++)
+    {
+        if(list[i].isEmpty==0)
+        {
+            showEmployeeSalary(list[i]);
+            flag=1;
+        }
+    }
+    if(flag==0)
+    {
+        //printf("---- don´t find Employees ----- \n");
+        printf("----- no employees found ----- \n");
+    }
+    printf("\n\n");
 }
 void printEmployees(Employee list[],int len)//muesstra los empleados
 {
@@ -535,4 +635,41 @@ int modifyEmployee(Employee employees[], int len)
 
     return ok;
 
+}
+float totalSalary(Employee e[],int len){
+    float total;
+        for(int i=0;i<len;i++){
+            if(e[i].isEmpty==0){
+                    total=(float)(total + e[i].salary);
+                    }
+            }
+
+return total;
+}
+float getAverageSalary(Employee e[],int len){//promedio de salarios
+float average=0;
+ float total;
+int cont=0;
+       for(int i=0;i<len;i++){
+            if(e[i].isEmpty==0){
+                    cont++;
+                     total=(float)(total + e[i].salary);
+                    }
+            }
+
+            average=total / (float) cont;
+
+
+     return average;
+}
+int numExceedPromedio(Employee e[],int len){
+int cont=0;
+float average=getAverageSalary(e,len);
+     for(int i=0;i<len;i++){
+            if(e[i].isEmpty==0 && (e[i].salary > average)){
+                    cont++;
+                    }
+            }
+
+return cont;
 }
